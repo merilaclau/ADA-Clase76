67ad73acbf4e6e005d6cbf35cf18df6c99f354e3
Meri Laclau <merilaclau@gmail.com>
2020-09-01 22:00:51 -0300

    toDoList + PUT

---
+++ index.js
@@ -0,0 +1,67 @@
+const onload = () => {
+    const lista = document.querySelector("#todo-list");
+    /*axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then(res => {
+            axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(res2 => {
+                res.data.forEach(todo => {
+                    const li = document.createElement('li');
+                    const title = document.createTextNode(todo.title);
+                    const u = res2.data.find(user => user.id === todo.userId);
+                    const user = document.createTextNode(u.name);
+                    const separator = document.createTextNode(" - ");
+                    li.appendChild(title);
+                    li.appendChild(separator);
+                    li.appendChild(user);
+                    lista.appendChild(li);
+                });
+            })
+        });*/
+
+
+    const p1 = axios.get("https://jsonplaceholder.typicode.com/todos");
+    const p2 = axios.get("https://jsonplaceholder.typicode.com/users");
+
+    Promise.all([p1, p2]).then(res => {
+        // res es un array [{data:}, {data:}]\
+        const todos = res[0].data;
+        const users = res[1].data;
+
+        todos.forEach(todo => {
+            const li = document.createElement('li');
+            const title = document.createTextNode(todo.title);
+            const u = users.find(user => user.id === todo.userId);
+            const user = document.createTextNode(u.name);
+            const separator = document.createTextNode(" - ");
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = todo.completed;
+
+            checkbox.addEventListener("click", () => {
+                    axios.put(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
+                        completed: checkbox.checked,
+                    }).then(res => {
+                        li.classList.toggle("completed");
+                    })
+                    .catch(err => {throw err;});
+                });
+            
+            li.appendChild(title);
+            li.appendChild(separator);
+            li.appendChild(user);
+            li.appendChild(checkbox);
+
+            if(todo.completed) li.classList.add("completed");
+            lista.appendChild(li);
+        });
+    })    
+};
+
+
+
+
+
+
+
+
+
